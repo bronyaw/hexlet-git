9b56f0b1054f7fc22a5a764772edd5ffc6d5e1c7
bronyaw <osusher@yahoo.com>
2022-12-01 01:25:02 +0300

    even_more_pytest, added README

---
+++ hexlet_pytest/tests/__init__.py
@@ -0,0 +1,12 @@
+
+from collections import defaultdict
+
+
+def get_index_sum(lst, target):
+    elements_counter = {}
+    for index, item in enumerate(lst):
+        elements_counter[item] = index
+    for 
+print(get_index_sum([1, 0, 2 , 4, 9], 6))
+    
+        
---
+++ hexlet_pytest/tests/test_emptiness.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+
+    stack.pop
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
